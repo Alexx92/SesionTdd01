893db753d523f44b4a35d273562e654d83464f78
Hernan <alexx92.04@gmail.com>
2016-04-27 11:44:34 -0300

    Commit sumar

--- src/test/java/cl/ubb/seccionTDD01/CalculadoraTest.java
+++ src/test/java/cl/ubb/seccionTDD01/CalculadoraTest.java
@@ -1,14 +1,22 @@
 package cl.ubb.seccionTDD01;
 
+import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import cl.ubb.agil.Calculator;
+
 public class CalculadoraTest {
 
-	@Test
-	public void test() {
-		fail("Not yet implemented");
+	@Test	
+	public void DosMasTresCinco(){
+		/*Arrange*/		
+		Calculadora calculo = new Calculadora();
+		/*Act*/
+		int resultado= calculo.sumar(2,2);
+		/*Assert*/
+		assertThat(resultado,is(4));
 	}
 
 }
