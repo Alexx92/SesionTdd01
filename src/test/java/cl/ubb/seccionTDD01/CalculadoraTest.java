9465373914a7ca442c78a52f52e858bd631948f3
Hernan <alexx92.04@gmail.com>
2016-04-27 12:05:29 -0300

    Sumar -1 mas 7 igual 6 FALLA

--- src/test/java/cl/ubb/seccionTDD01/CalculadoraTest.java
+++ src/test/java/cl/ubb/seccionTDD01/CalculadoraTest.java
@@ -3,20 +3,21 @@ package cl.ubb.seccionTDD01;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
+
 
 import cl.ubb.seccionTDD01.Calculadora;
 
 public class CalculadoraTest {
-
-	@Test	
-	public void DosMasTresCinco(){
-		/*Arrange*/		
+	@Test
+	public void menos1MasSieteEsSeis(){
+		/*Arrange*/
 		Calculadora calculo = new Calculadora();
 		/*Act*/
-		int resultado= calculo.sumar(2,2);
+		int resultado=calculo.sumar(-1,7);
 		/*Assert*/
-		assertThat(resultado,is(5));
+		assertThat(resultado,is(6));
 	}
 
 }
